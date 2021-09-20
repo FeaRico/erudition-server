24babad98394f89060382e348d606dbf9a4a2cc7
Mahach Abdulazizov <abdulazizov2001@mail.ru>
2021-09-20 03:32:12 +0300

    Add to Answer entity

--- src/main/java/ru/mahach/eruditionserver/models/Answer.java
+++ src/main/java/ru/mahach/eruditionserver/models/Answer.java
@@ -1,2 +1,35 @@
-package ru.mahach.eruditionserver.models;public class Answer {
+package ru.mahach.eruditionserver.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "answers")
+public class Answer {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "answers_id_seq"
+    )
+    @SequenceGenerator(
+            name = "answers_id_seq",
+            sequenceName = "answers_id_seq"
+    )
+    @Column(
+            name = "id",
+            nullable = false,
+            unique = true,
+            insertable = false,
+            updatable = false
+    )
+    private Long id;
+
+    @Column(
+            name = "text"
+    )
+    private String text;
+
+    @Column(
+            name = "question"
+    )
+    private Long question;
 }
