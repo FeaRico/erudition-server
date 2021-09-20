c575546e24f28c99c005875d85abc0e9255e58c5
Mahach Abdulazizov <abdulazizov2001@mail.ru>
2021-09-20 13:01:36 +0300

    Add ItemServiceImpl

--- src/main/java/ru/mahach/eruditionserver/services/impl/ItemServiceImpl.java
+++ src/main/java/ru/mahach/eruditionserver/services/impl/ItemServiceImpl.java
@@ -1,2 +1,54 @@
-package ru.mahach.eruditionserver.services.impl;public class ItemServiceImpl {
+package ru.mahach.eruditionserver.services.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.mahach.eruditionserver.exceptions.ItemNotFoundException;
+import ru.mahach.eruditionserver.models.Item;
+import ru.mahach.eruditionserver.repository.ItemRepository;
+import ru.mahach.eruditionserver.services.ItemService;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class ItemServiceImpl implements ItemService {
+
+    private final ItemRepository itemRepository;
+
+    ItemServiceImpl(ItemRepository itemRepository){
+        this.itemRepository = itemRepository;
+    }
+
+    @Override
+    public Optional<Item> createItem(Item item) {
+        return Optional.of(itemRepository.save(item));
+    }
+
+    @Override
+    public Optional<Item> updateItem(Item item) {
+        Item itemToUpdate = itemRepository.findById(item.getId())
+                .orElseThrow(() -> new ItemNotFoundException(item.getId()));
+        return Optional.of(itemRepository.save(itemToUpdate));
+    }
+
+    @Override
+    public Optional<Item> deleteItemById(Long id) {
+        Item itemToDelete = itemRepository.findById(id)
+                .orElseThrow(() -> new ItemNotFoundException(id));
+        itemRepository.deleteById(id);
+        return Optional.of(itemToDelete);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Item> itemFindById(Long id) {
+        return itemRepository.findById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Collection<Item> itemFindAll() {
+        return itemRepository.findAll();
+    }
 }
