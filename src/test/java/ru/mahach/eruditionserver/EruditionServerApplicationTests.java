f56096410fa3d2a65ddb9b810a1bfc698452fb8e
Mahach Abdulazizov <abdulazizov2001@mail.ru>
2021-09-20 03:06:28 +0300

    Init commit

---
+++ src/test/java/ru/mahach/eruditionserver/EruditionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mahach.eruditionserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EruditionServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
