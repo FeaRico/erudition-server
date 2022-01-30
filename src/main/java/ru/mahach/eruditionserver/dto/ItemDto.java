335706fdeda526bed1fd0cd7f585ec5ad2f30330
Makhach Abdulazizov <abdulazizov2001@mail.ru>
2022-01-30 14:07:05 +0300

    set immutable dto & and builder for them

--- src/main/java/ru/mahach/eruditionserver/dto/ItemDto.java
+++ src/main/java/ru/mahach/eruditionserver/dto/ItemDto.java
@@ -1,6 +1,9 @@
 package ru.mahach.eruditionserver.dto;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.util.Objects.isNull;
 
 public class ItemDto {
     private Long id;
@@ -8,7 +11,7 @@ public class ItemDto {
     private String imagePath;
     private List<QuestionDto> questions;
 
-    public ItemDto(Long id, String name, String imagePath, List<QuestionDto> questions) {
+    ItemDto(Long id, String name, String imagePath, List<QuestionDto> questions) {
         this.id = id;
         this.name = name;
         this.imagePath = imagePath;
@@ -29,5 +32,42 @@ public class ItemDto {
 
     public List<QuestionDto> getQuestions() {
         return questions;
+    }
+
+    public static class ItemDtoBuilder {
+        private Long id;
+        private String name;
+        private String imagePath;
+        private List<QuestionDto> questions;
+
+        ItemDtoBuilder() {
+        }
+
+        public ItemDtoBuilder setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public ItemDtoBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public ItemDtoBuilder setImagePath(String imagePath) {
+            this.imagePath = imagePath;
+            return this;
+        }
+
+        public ItemDtoBuilder setQuestions(List<QuestionDto> questions) {
+            if (isNull(questions))
+                questions = new ArrayList<>();
+
+            this.questions = questions;
+            return this;
+        }
+
+        public ItemDto build() {
+            return new ItemDto(id, name, imagePath, questions);
+        }
     }
 }
--- src/main/java/ru/mahach/eruditionserver/dto/QuestionDto.java
+++ src/main/java/ru/mahach/eruditionserver/dto/QuestionDto.java
@@ -1,6 +1,9 @@
 package ru.mahach.eruditionserver.dto;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.util.Objects.isNull;
 
 public class QuestionDto {
     private Long id;
@@ -9,7 +12,7 @@ public class QuestionDto {
     private String imagePath;
     private List<AnswerDto> answers;
 
-    public QuestionDto(Long id, String text, Long itemId, String imagePath, List<AnswerDto> answers) {
+    QuestionDto(Long id, String text, Long itemId, String imagePath, List<AnswerDto> answers) {
         this.id = id;
         this.text = text;
         this.itemId = itemId;
@@ -35,5 +38,48 @@ public class QuestionDto {
 
     public List<AnswerDto> getAnswers() {
         return answers;
+    }
+
+    public static class QuestionDtoBuilder {
+        private Long id;
+        private String text;
+        private Long itemId;
+        private String imagePath;
+        private List<AnswerDto> answers;
+
+        QuestionDtoBuilder() {
+        }
+
+        QuestionDtoBuilder setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        QuestionDtoBuilder setText(String text) {
+            this.text = text;
+            return this;
+        }
+
+        QuestionDtoBuilder setItemId(Long itemId) {
+            this.itemId = itemId;
+            return this;
+        }
+
+        QuestionDtoBuilder setImagePath(String imagePath) {
+            this.imagePath = imagePath;
+            return this;
+        }
+
+        QuestionDtoBuilder setAnswers(List<AnswerDto> answers) {
+            if (isNull(answers))
+                answers = new ArrayList<>();
+
+            this.answers = answers;
+            return this;
+        }
+
+        QuestionDto build() {
+            return new QuestionDto(id, text, itemId, imagePath, answers);
+        }
     }
 }
